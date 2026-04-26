09cc35100ef1788ae45629d3e160f84c07ba60ab
Adam Jackson <vcs@ahejackson.dev>
2026-04-26 15:04:43 +0100

    Update pla-ivs.ts

--- src/components/pla-ivs.ts
+++ src/components/pla-ivs.ts
@@ -1,7 +1,6 @@
 import { LitElement, html, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { classMap } from "lit/directives/class-map.js";
-import { styleMap } from "lit/directives/style-map.js";
 
 import type { IVs } from "../types";
 import { Nature } from "../types";
